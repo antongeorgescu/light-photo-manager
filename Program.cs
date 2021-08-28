e8c68b882a7e13177515050afa72a1c1a00d9888
Anton Georgesco <antongeorgescu@gmail.com>
2021-08-28 16:54:11 -0400

    first commit

---
+++ Program.cs
@@ -0,0 +1,103 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Light_Photo_Manager
+{
+    
+    public class RootObject
+    {
+        public Path[] Paths { get; set; }
+    }
+
+    public class Path
+    {
+        public string Type { get; set; }
+        public string Dir { get; set; }
+        public bool Exists { get; set; }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            RootObject setts;
+            using (StreamReader r = new StreamReader("configuration.json"))
+            {
+                string json = r.ReadToEnd();
+                setts = JsonConvert.DeserializeObject<RootObject>(json);
+            }
+
+            // extract destination paths
+            List<string> destPaths = new List<string>();
+            foreach (var sett in setts.Paths)
+            {
+                // check if directory exists
+                if (!Directory.Exists($"{sett.Dir}"))
+                {
+                    if (sett.Type == "src")
+                        Console.WriteLine($"[{DateTime.Now}] Source directory {sett.Dir} does not exist and will be skipped. ");
+                    else
+                        Console.WriteLine($"[{DateTime.Now}] Destination directory {sett.Dir} does not exist and will be skipped. ");
+                    sett.Exists = false;
+                }
+                else
+                    sett.Exists = true;
+
+                if (sett.Type == "dest")
+                    destPaths.Add(sett.Dir);
+            }
+
+            // process the source paths
+            Console.WriteLine($"[{DateTime.Now}] Light_Photo_Manager started to parse the source folders...");
+            foreach (var sett in setts.Paths)
+            {
+                if (!sett.Exists) continue;
+                if (sett.Type == "src") {
+                    try
+                    {
+                        string[] files = Directory.GetFiles(sett.Dir, "*", SearchOption.AllDirectories);
+                        
+                        Console.WriteLine($"[{DateTime.Now}] {files.Length} files are getting processed in source folder {sett.Dir}...");
+                        foreach (var file in files)
+                        {
+                            FileInfo fi = new FileInfo(file);
+                            var created = fi.CreationTime;
+                            var lastmodified = fi.LastWriteTime;
+
+                            // get Year of file creation
+                            var year = created.Year;
+
+                            // copy the file
+                            foreach (var path in destPaths) {
+                                // if destination directory does not exist, display error and exit  the for loop
+                                if (!Directory.Exists($"{path}"))
+                                {
+                                    continue;
+                                }
+
+                                // check if Year existed as subfolder in destination folders; if not create it
+                                if (!Directory.Exists($"{path}/{year}"))
+                                    Directory.CreateDirectory($"{path}/{year}");
+                                // copy the file in respective subfolder of destination path
+                                File.Copy(file, $"{path}//{year}//{fi.Name}",true);
+                            }
+                        }
+                        Console.WriteLine($"[{DateTime.Now}] Source folder {sett.Dir} has been fully processed.");
+                        
+                    }
+                    catch(Exception ex)
+                    {
+                        Console.WriteLine($"[{DateTime.Now}] Error: {ex.Message}");
+                    }
+                    
+
+
+                }
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
